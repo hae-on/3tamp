656f7c0b69e262c24feabbe626f98a2be09129d4
hae-on <solbi2004@naver.com>
2021-12-01 14:57:00 +0900

    [add]router

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,18 @@
-import "./app.module.css";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./main/main";
+import Home from "./home/home";
+import "./app.module.css";
 
 function App() {
-  return <Main />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
