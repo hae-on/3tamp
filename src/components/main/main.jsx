3fc2ba4af02dd005d3346645ba82a282704eaa61
hae-on <solbi2004@naver.com>
2021-12-02 22:05:49 +0900

    [add]firebase setting

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./main.module.css";
 import Snow from "../snow/snow";
 
 const Main = (props) => {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate("/home");
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.background}>
         <Snow />
-        <button className={styles.start_btn}>🧡 💛 3tamp 시작하기 💛 🧡</button>
+        <button className={styles.start_btn} onClick={onClick}>
+          🧡 💛 3tamp 시작하기 💛 🧡
+        </button>
       </div>
     </div>
   );
