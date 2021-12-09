1474113936b95389d66ab9a7306273ed1e1f2b71
hae-on <solbi2004@naver.com>
2021-12-09 22:22:24 +0900

    [add]modal 뒷 배경 추가

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -6,28 +6,30 @@ import ColorBox from "../colorBox/colorBox";
 
 const Modal = ({ modalClose }) => {
   return (
-    <div className={styles.modal}>
-      <span className={styles.intro}>새로운 습관을 만들어볼까요?</span>
-      <button onClick={modalClose} className={styles.close_btn}>
-        <FaTimes className={styles.x} />
-      </button>
-      <form className={styles.form}>
-        <div className={styles.title}>
-          <label>제목 </label>
-          <input
-            type="text"
-            className={styles.title_input}
-            maxLength="15"
-          ></input>
-        </div>
-        <div className={styles.color}>
-          <label className={styles.color_label}>색상 </label>
-          <ColorBox />
-        </div>
-        <button type="submit" className={styles.check_btn}>
-          <AiFillCheckCircle className={styles.check} />
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <span className={styles.intro}>새로운 습관을 만들어볼까요?</span>
+        <button onClick={modalClose} className={styles.close_btn}>
+          <FaTimes className={styles.x} />
         </button>
-      </form>
+        <form className={styles.form}>
+          <div className={styles.title}>
+            <label>제목 </label>
+            <input
+              type="text"
+              className={styles.title_input}
+              maxLength="15"
+            ></input>
+          </div>
+          <div className={styles.color}>
+            <label className={styles.color_label}>색상 </label>
+            <ColorBox />
+          </div>
+          <button type="submit" className={styles.check_btn}>
+            <AiFillCheckCircle className={styles.check} />
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
