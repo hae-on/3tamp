bc756054d5abd07b56a3e607bc98fe5e46a827d7
hae-on <solbi2004@naver.com>
2021-12-10 19:57:38 +0900

    [add]box color 설정

--- src/components/hardModeBox/hardModeBox.jsx
+++ src/components/hardModeBox/hardModeBox.jsx
@@ -3,32 +3,14 @@ import styles from "./hardModeBox.module.css";
 import btn from "../../img/logo.png";
 
 const HardModeBox = ({ hardBox }) => {
-  //   const { title, color } = hardBox;
-  const color = [
-    { key: "red", value: "#ff98a5" },
-    { key: "orenge", value: "#ffdcb0" },
-    { key: "yellow", value: "#fff88d" },
-    { key: "green", value: "#b9ffbd" },
-    { key: "blue", value: "#ace8ff" },
-    { key: "purple", value: "#d4b4ff" },
-    { key: "pink", value: "#ffd1d5" },
-  ];
-
-  // const getRandomColor = () => {
-  //   return color[Math.floor(Math.random() * color.length)];
-  // };
+  const { title, color } = hardBox;
 
   return (
     <>
-      <div
-        className={styles.box}
-        style={{
-          background: "pink",
-        }}
-      >
+      <div className={`${styles.box} ${getStyles(color)}`}>
         <div className={styles.head}>
           <form>
-            <h2 className={styles.title}> {hardBox.title} </h2>
+            <h1 className={styles.title}> {title} </h1>
           </form>
           <button className={styles.btn}>
             <img src={btn} alt="btn" className={styles.btn_img} />
@@ -69,5 +51,26 @@ const HardModeBox = ({ hardBox }) => {
     </>
   );
 };
+
+function getStyles(color) {
+  switch (color) {
+    case "red":
+      return styles.red;
+    case "orange":
+      return styles.orange;
+    case "yellow":
+      return styles.yellow;
+    case "green":
+      return styles.green;
+    case "blue":
+      return styles.blue;
+    case "purple":
+      return styles.purple;
+    case "pink":
+      return styles.pink;
+    default:
+      throw new Error(`unknown color: ${color}`);
+  }
+}
 
 export default HardModeBox;
