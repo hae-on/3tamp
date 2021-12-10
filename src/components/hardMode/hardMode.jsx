b7155b3f71473cc16979fd9aa5851d12eb386c91
hae-on <solbi2004@naver.com>
2021-12-10 19:02:00 +0900

    [add]dummy title 받아오기

--- src/components/hardMode/hardMode.jsx
+++ src/components/hardMode/hardMode.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./hardMode.module.css";
-import btn from "../../img/logo.png";
 import AddButton from "../addButton/addButton";
 import HardModeBox from "../hardModeBox/hardModeBox";
 
 const HardMode = () => {
+  const [hardBoxes, setHardBoxes] = useState([
+    {
+      id: "1",
+      title: "html 공부하기",
+      color: "red",
+    },
+    {
+      id: "2",
+      title: "영어 단어 외우기",
+      color: "purple",
+    },
+  ]);
+
   return (
     <>
-      <HardModeBox />
-      <AddButton />
+      {hardBoxes.map((hardBox) => (
+        <HardModeBox hardBox={hardBox} />
+      ))}
+
+      <AddButton hardBoxes={hardBoxes} />
     </>
   );
 };
--- src/components/hardModeBox/hardModeBox.jsx
+++ src/components/hardModeBox/hardModeBox.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import styles from "./hardModeBox.module.css";
 import btn from "../../img/logo.png";
-import AddButton from "../addButton/addButton";
 
-const HardModeBox = () => {
-  // const color = [
-  //   "#ff98a5",
-  //   "#ffdcb0",
-  //   "#fff88d",
-  //   "#b9ffbd",
-  //   "#ace8ff",
-  //   "#d4b4ff",
-  //   "#ffd1d5",
-  // ];
+const HardModeBox = ({ hardBox }) => {
+  //   const { title, color } = hardBox;
+  const color = [
+    { key: "red", value: "#ff98a5" },
+    { key: "orenge", value: "#ffdcb0" },
+    { key: "yellow", value: "#fff88d" },
+    { key: "green", value: "#b9ffbd" },
+    { key: "blue", value: "#ace8ff" },
+    { key: "purple", value: "#d4b4ff" },
+    { key: "pink", value: "#ffd1d5" },
+  ];
 
   // const getRandomColor = () => {
   //   return color[Math.floor(Math.random() * color.length)];
@@ -22,18 +22,13 @@ const HardModeBox = () => {
     <>
       <div
         className={styles.box}
-        // style={{
-        //   background: getRandomColor(color),
-        // }}
+        style={{
+          background: "pink",
+        }}
       >
         <div className={styles.head}>
           <form>
-            <input
-              className={styles.title}
-              type="text"
-              placeholder="제목을 입력해주세요"
-              maxLength="15"
-            ></input>
+            <h2 className={styles.title}> {hardBox.title} </h2>
           </form>
           <button className={styles.btn}>
             <img src={btn} alt="btn" className={styles.btn_img} />
