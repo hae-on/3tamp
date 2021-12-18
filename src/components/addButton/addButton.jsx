d1abdf9381cc71c62972fd2a5539fa1c813c7b20
hae-on <solbi2004@naver.com>
2021-12-18 21:48:57 +0900

    [add]delete modal style

--- src/components/addButton/addButton.jsx
+++ src/components/addButton/addButton.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { AiFillPlusCircle } from "react-icons/ai";
 import styles from "./addButton.module.css";
 import AddBox from "../addBox/addBox";
-import DeleteOrCompleteModal from "../deleteOrCompleteModal/deleteOrCompleteModal";
 
 const AddButton = ({ addHardBox }) => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -15,9 +14,7 @@ const AddButton = ({ addHardBox }) => {
       <button onClick={modalClose} className={styles.btn}>
         <AiFillPlusCircle className={styles.add_icon} />
       </button>
-      {modalOpen && <DeleteOrCompleteModal modalClose={modalClose} />}
-
-      {/* //<AddBox onAdd={addHardBox} modalClose={modalClose} />} */}
+      {modalOpen && <AddBox onAdd={addHardBox} modalClose={modalClose} />}
     </>
   );
 };
--- src/components/deleteOrCompleteModal/deleteOrCompleteModal.jsx
+++ src/components/deleteOrCompleteModal/deleteOrCompleteModal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./deleteOrCompleteModal.module.css";
 import { FaTimes } from "react-icons/fa";
+import bear from "../../img/bear.png";
 
 const DeleteOrCompleteModal = ({ modalClose }) => {
   return (
@@ -9,6 +10,24 @@ const DeleteOrCompleteModal = ({ modalClose }) => {
         <button className={styles.close_btn} onClick={modalClose}>
           <FaTimes className={styles.x} />
         </button>
+        <div className={styles.bear_img}>
+          <img src={bear} alt="bear" className={styles.bear} />
+        </div>
+        <p className={styles.complete_title}>
+          도장을
+          <span className={styles.complete_word}>완료</span>
+          하시겠어요?
+        </p>
+        <p className={styles.delete_title}>
+          혹은 <span className={styles.delete_word}>삭제</span>하시겠어요?
+        </p>
+        <p className={styles.warning}>
+          ⚠️ 완료 또는 삭제 후 복구할 수 없으니 신중하게 눌러주세요!
+        </p>
+        <div className={styles.btn_container}>
+          <button className={styles.complete_btn}>완료</button>
+          <button className={styles.delete_btn}>삭제</button>
+        </div>
       </div>
     </div>
   );
