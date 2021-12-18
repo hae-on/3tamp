586b24e71829aace406822824e3feacf215d39ff
hae-on <solbi2004@naver.com>
2021-12-18 23:44:07 +0900

    스타일 수정

--- src/components/deleteOrCompleteModal/deleteOrCompleteModal.jsx
+++ src/components/deleteOrCompleteModal/deleteOrCompleteModal.jsx
@@ -17,14 +17,16 @@ const DeleteOrCompleteModal = ({ modalClose, hardBox, deleteHardBox }) => {
         <div className={styles.bear_img}>
           <img src={bear} alt="bear" className={styles.bear} />
         </div>
-        <p className={styles.complete_title}>
-          도장을
-          <span className={styles.complete_word}>완료</span>
-          하시겠어요?
-        </p>
-        <p className={styles.delete_title}>
-          혹은 <span className={styles.delete_word}>삭제</span>하시겠어요?
-        </p>
+        <div className={styles.title}>
+          <p className={styles.complete_title}>
+            도장을
+            <span className={styles.complete_word}>완료</span>
+            하시겠어요?
+          </p>
+          <p className={styles.delete_title}>
+            혹은 <span className={styles.delete_word}>삭제</span>하시겠어요?
+          </p>
+        </div>
         <p className={styles.warning}>
           ⚠️ 완료 또는 삭제 후 복구할 수 없으니 신중하게 눌러주세요!
         </p>
