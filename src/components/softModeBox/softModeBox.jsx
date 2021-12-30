0440fe1495be4776fd947e134b33418f0f436227
hae-on <solbi2004@naver.com>
2021-12-30 14:44:27 +0900

    soft 도장 변경

--- src/components/softModeBox/softModeBox.jsx
+++ src/components/softModeBox/softModeBox.jsx
@@ -3,8 +3,8 @@ import styles from "../hardModeBox/modeBox.module.css";
 import btn from "../../img/logo.png";
 import DeleteOrCompleteModal from "../deleteOrCompleteModal/deleteOrCompleteModal";
 import moment from "moment";
-import o from "../../img/hard_o.png";
-import x from "../../img/hard_x.png";
+import o from "../../img/soft_o.jpg";
+import x from "../../img/soft_x.jpg";
 
 const SoftModeBox = ({ softBox, updatesoftBox, deletesoftBox }) => {
   const { title, color, startDate } = softBox;
